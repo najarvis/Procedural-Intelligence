6275ab76b30fcc75ae93015a6072a2338d5420ae
Nick Jarvis <najarvis2016@gmail.com>
2017-05-24 14:02:48 +0200

    Finishes up gallery viewing as well as add a footer navigation bar and a 404 page

--- procint.py
+++ procint.py
@@ -24,6 +24,10 @@ def default():
 def special_creator():
     return render_template("special_creator.html")
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
 
