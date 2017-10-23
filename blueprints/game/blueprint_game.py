0c5f5711b59d144697257cf725d83071d75b66fa
Nick Jarvis <najarvis2016@gmail.com>
2017-10-23 13:12:49 -0400

    Changes game to pacman game.

--- blueprints/game/blueprint_game.py
+++ blueprints/game/blueprint_game.py
@@ -1,14 +1,20 @@
-from flask import Blueprint, render_template, request, url_for
+from flask import Blueprint, render_template, request, url_for, send_from_directory
 
-blueprint_game = Blueprint('game', __name__, template_folder='templates', static_folder='static')
+blueprint_game = Blueprint('game', __name__, template_folder='pacmanjs-e', static_url_path='')
 
 @blueprint_game.route('/game')
 def view_game():
-    return render_template("game.html")
+    return render_template("pages/index.html")
 
-@blueprint_game.route('/grab_script/<script>')
-def get_script(script=None):
-    if script is None:
-        return
+@blueprint_game.route('/<path:file>')
+def get_file(file):
+	if file is not None:
+		return send_from_directory(blueprint_game.root_path+'/pacmanjs-e', file)
 
-    return render_template('scripts/'+script)
+
+# @blueprint_game.route('/grab_script/<script>')
+# def get_script(script=None):
+#     if script is None:
+#         return
+
+#     return render_template('scripts/'+script)
