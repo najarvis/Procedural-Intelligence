0e514f7750276246da94fb731698ca53ea844545
Nick Jarvis <najarvis2016@gmail.com>
2017-12-06 05:55:41 +0000

    Updates and working with the pacman project

--- blueprints/game/blueprint_game.py
+++ blueprints/game/blueprint_game.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template, request, url_for, send_from_directory
+from flask import Blueprint, render_template, request, url_for, send_from_directory, jsonify
+import json
 
 blueprint_game = Blueprint('game', __name__, template_folder='pacmanjs-e', static_url_path='')
 
@@ -11,6 +12,17 @@ def get_file(file):
 	if file is not None:
 		return send_from_directory(blueprint_game.root_path+'/pacmanjs-e', file)
 
+@blueprint_game.route('/get_high_scores')
+def get_high_scores():
+    data = json.load(open(blueprint_game.root_path+'/pacmanjs-e/high_scores.json'))
+    return jsonify(data)
+
+@blueprint_game.route('/update_high_scores', methods=['POST'])
+def update_high_scores():
+    data = request.get_json()
+    with open(blueprint_game.root_path+'/pacmanjs-e/high_scores.json', 'w') as f:
+        json.dump(data, f)
+    return jsonify(data)
 
 # @blueprint_game.route('/grab_script/<script>')
 # def get_script(script=None):
